6a4d7d529d57b959d4d911f359623f2a0c875a52
AndrewKostin <andrewkostin255@ya.ru>
2023-03-17 02:31:23 +0700

    userProfile changes + added modalWindow + some changes

--- frontend/src/pages/userprofile/UserProfile.jsx
+++ frontend/src/pages/userprofile/UserProfile.jsx
@@ -1,11 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import Api from "../../API/Api";
+import settingsButton from "./../../UI/img/settings.png"
 import classes from "./userprofile.module.css";
-import {UserHeader} from "../../components/header/UserHeader"
 import {MyFooter} from "../../components/footer/MyFooter";
 import {ImgComponent} from "../../components/ImgComponent/ImgComponent";
+import {PluginViewHeader} from "../../components/header/PluginViewHeader";
+import ModalWindow from "../../components/modalWindow/ModalWindow";
+
 
 const UserProfile = () => {
+    const [visibleModal, setVisibleModal] = useState(false);
+
     const [userData, setUserData] = useState({
         active: true,
         description: '',
@@ -30,11 +35,20 @@ const UserProfile = () => {
             console.log(userData);
         }
 
-        },[disable]);
+    },[disable]);
+
+    const sendData = () =>{
+        Api.changeUserData(userData)
+            .then((response) => {console.log(response)})
+            .catch((response) => {console.log(response)});
+        console.log(userData);
+        setVisibleModal(false);
+    }
 
     return (
         <div>
-            <UserHeader/>
+
+            <PluginViewHeader/>
             <div className={[classes.my__profile, "container"].join(' ')}>
                 <div className={"row"}>
                     <div className={["col-1", classes.my__menu].join(' ')}>
@@ -50,67 +64,54 @@ const UserProfile = () => {
                         <div className={classes.my__img}>
                             <ImgComponent func={Api.previewByPluginId(0)}></ImgComponent>
                         </div>
-{/*                        <div className={classes.my__changeImg}>
-                            change
-                        </div>*/}
                     </div>
 
-                    <div className={["col-6", classes.my__info].join(' ')}>
-                        <form id="the_form">
-                            <div className={classes.my__name}>
-                                {(disable === null) &&
-                                    <div className={classes.my__nameText}>
-                                        name
-                                    </div>
-                                }
-                                <textarea className={classes.my__nameArea} value={userData.nickName}
-                                          disabled={disable} onChange={event =>
-                                    setUserData({...userData, nickName: event.target.value})}/>
+                    <div className={["col-5", classes.my__info].join(' ')}>
+                        <div className={classes.my__nicknameInfo}>
+                            <div className={classes.my__nicknameText}>
+                                Nickname:
                             </div>
+                            <div className={classes.my__nickname}>
+                                {userData.nickName}
+                            </div>
+                        </div>
 
-
+                        <div className={classes.my__mailInfo}>
+                            <div className={classes.my__mailText}>
+                                Mail:
+                            </div>
                             <div className={classes.my__mail}>
-                                {(disable === null) &&
-                                    <div className={classes.my__mailText}>
-                                        mail
-                                    </div>
-                                }
-                                <textarea className={classes.my__mailArea} value={userData.email}
-                                          disabled={disable} onChange={event =>
-                                    setUserData({...userData, email: event.target.value})}/>
+                                {userData.email}
                             </div>
+                        </div>
 
+                        <div className={classes.my__contactsInfo}>
+                            <div className={classes.my__contactsText}>
+                                Contacts:
+                            </div>
                             <div className={classes.my__contacts}>
-                                {(disable === null) &&
-                                    <div className={classes.my__contactsText}>
-                                        contacts
-                                    </div>
-                                }
-
-
-                                <textarea className={classes.my__contactsArea} value={userData.phoneNumber}
-                                          disabled={disable} onChange={event =>
-                                    setUserData({...userData, phoneNumber: event.target.value})}/>
+                                {userData.phoneNumber}
                             </div>
+                        </div>
 
+                        <div className={classes.my__descriptionInfo}>
+                            <div className={classes.my__descriptionText}>
+                                Description:
+                            </div>
                             <div className={classes.my__description}>
-                                {(disable === null) &&
-                                    <div className={classes.my__descriptionText}>
-                                        description
-                                    </div>
-                                }
-
-                                <textarea className={classes.my__descriptionArea} value={userData.description}
-                                          disabled={disable} onChange={event =>
-                                    setUserData({...userData, description: event.target.value})}/>
+                                {userData.description}
                             </div>
-
-                            <button type={"button"} className={classes.my__button}
-                                    onClick={_ => setDisable(disable !== null ? null : "disable")}>
-                                {disable !== null ? "edit" : "apply"}
-                            </button>
-                        </form>
+                        </div>
                     </div>
+
+                    <div onClick={() => setVisibleModal(true)} className={["col-1", classes.my__button].join(' ')}>
+                        <img className={classes.my__settingsButton}
+                             src={settingsButton}
+                             alt={".."}/>
+                    </div>
+                    {visibleModal ?
+                        <ModalWindow accept={() => {sendData()}} decline={() => {setVisibleModal(false)}}/>
+                            : <></>}
                 </div>
             </div>
             <MyFooter/>
