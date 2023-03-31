51bdcdc55a91acf810081552e9eb0a327a7c8ca4
Nikita <iniktus@.mail.ru>
2023-03-31 12:28:59 +0700

    Добавил зависимость для вебклиента (вдруг пригодится) и сделал заготовку класса для взаимодействия с сервером личного кабинета дарси. Потом допишу.

---
+++ server/src/main/java/com/store/gs/configuration/DarcyConfiguration.java
@@ -0,0 +1,13 @@
+package com.store.gs.configuration;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@Setter
+@Getter
+public class DarcyConfiguration {
+}
