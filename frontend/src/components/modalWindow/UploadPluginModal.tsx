56765544c3361ade5bcc6fc4bd52594492d20966
PlesneviyGRIB <ustoppablerina@gmail.com>
2023-04-29 09:47:26 +0700

    fix issues

--- frontend/src/components/modalWindow/UploadPluginModal.tsx
+++ frontend/src/components/modalWindow/UploadPluginModal.tsx
@@ -3,7 +3,8 @@ import {Styled as S} from "./../default/Modal.styled";
 import {Modal} from "../default/Modal";
 import Api from "../../API/Api";
 import {Input, TextArea} from "../default/Form";
-import classes from "./modalwindow.module.css";
+import {Categories} from "../header/category/Categories";
+import {Column, FlexRow} from "../default/Flex.styled";
 interface IUploadPluginModal {
     opened: boolean;
     setOpened: (state : boolean) => void;
@@ -33,35 +34,39 @@ export const UploadPluginModal : React.FC<IUploadPluginModal> = ({setOpened}) =>
     }, [pluginData, setOpened])
 
     const handleSetCategory = (categoryId: number) => {
-        setPluginData({... pluginData, categories: categoryId});
+        //setPluginData({... pluginData, categories: categoryId});
     }
 
     return (
         <Modal
-            $height={"1000px"}
-            $width={"900px"}
+            $height={"900px"}
+            $width={"1000px"}
             onAccept={handleAcceptModal}
             onDecline={handleCloseModal}
             onClose={handleCloseModal}
         >
             <S.Title>UPLOAD NEW PLUGIN</S.Title>
             <S.Body>
-                <div className={classes.my__label}>Name:</div>
-                <Input type={"text"} value={pluginData.name}
-                       onChange={(e) => {setPluginData({... pluginData, name: e.target.value})}}/>
-                <div className={classes.my__label}>Short description:</div>
-                <Input type={"text"} value={pluginData.shortDescription}
-                       onChange={(e) => {setPluginData({... pluginData, shortDescription: e.target.value})}}/>
-                <div className={classes.my__label}>Full description:</div>
-                <Input type={"text"} value={pluginData.fullDescription}
-                       onChange={(e) => {setPluginData({... pluginData, fullDescription: e.target.value})}}/>
-                <div className={classes.my__label}>Price:</div>
-                <TextArea type={"text"} value={pluginData.price}
-                          onChange={(e) => {setPluginData({... pluginData, price: e.target.value})}}/>
-
-                <Categories setCategory={handleSetCategory} category={pluginData.categories}/>
-                <input className={classes.my__fileLoader}
-                       type={"file"}/>
+                <FlexRow>
+                    <Column style={{width:"400px"}}>
+                        <S.Row>Name:</S.Row>
+                        <Input type={"text"} value={pluginData.name}
+                               onChange={(e) => {setPluginData({... pluginData, name: e.target.value})}}/>
+                        <S.Row>Short description:</S.Row>
+                        <Input type={"text"} value={pluginData.shortDescription}
+                               onChange={(e) => {setPluginData({... pluginData, shortDescription: e.target.value})}}/>
+                        <S.Row>Full description:</S.Row>
+                        <Input type={"text"} value={pluginData.fullDescription}
+                               onChange={(e) => {setPluginData({... pluginData, fullDescription: e.target.value})}}/>
+                        <S.Row>Price:</S.Row>
+                        <TextArea type={"text"} value={pluginData.price}
+                                  onChange={(e) => {setPluginData({... pluginData, price: e.target.value})}}/>
+                    </Column>
+                    <Column>
+                        <Categories setCategory={handleSetCategory} category={-1}/>
+                        <input type={"file"}/>
+                    </Column>
+                </FlexRow>
             </S.Body>
         </Modal>
     )
